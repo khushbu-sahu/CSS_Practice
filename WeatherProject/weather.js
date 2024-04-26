3bd7f3ee7468e3ac140a7c0dc5ca0fbe164d9e76
khushbu-sahu <khushboosahu15@outlook.com>
2024-04-26 14:14:32 +0530

    music player

--- WeatherProject/weather.js
+++ WeatherProject/weather.js
@@ -41,10 +41,6 @@ async function checkWeather(city) {
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
 
-
-
-
-
     }
 }
 
